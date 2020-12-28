6d5f6dc545147e71a97d0f4b8811ecd7749be9ce
Blazederah <lifepuzzler1337@gmail.com>
2020-12-28 02:10:03 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,38 @@
-import os
+import discord
 from discord.ext import commands
+import random
+import asyncio
+import datetime
+from urllib import parse, request
+import re
+import os
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix='>', description="This is a Helper Bot")
+
+bot.remove_command('help')
+
+@bot.listen()
+async def on_message(message):
+	word_list = ['hello', 'hi', 'hey']
+
+	messageContent = message.content
+	if len(messageContent) > 0:
+		for word in word_list:
+			if word in messageContent:
+				await message.channel.send('Hello :)')
+				# await message.delete()
+
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    await bot.change_presence(activity=discord.Game('with your mom'))
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
 
-if __name__ == "__main__":
-    bot.run(TOKEN)
+
+bot.run(DISCORD_TOKEN)
